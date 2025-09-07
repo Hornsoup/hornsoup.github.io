ec52dd1e16001649f13299f33769718ddc13a78c
Hornsoup <109191012+Hornsoup@users.noreply.github.com>
2025-09-07 16:13:11 -0700

    Sidebar and data-test properties
    Various minor changes along with an added Sidebar and data-test for when I finally get around to doing deeper testing. Might change those up.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,12 +4,12 @@ test('has title', async ({ page }) => {
   await page.goto('./index.html');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Erin — Portfolio/);
+  await expect(page).toHaveTitle(/Portfolio/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('./PlaywrightTestSite/index.html');
-  await page.fill('#inputName', 'Erin');
+  await page.goto('./PlaywrightTestSite');
+  await page.fill('#inputName', 'Diver Dave');
   await page.click('button[data-test="submitName"]');
-  await expect(page.locator('#result')).toHaveText('Hello, Erin!');
+  await expect(page.locator('#result')).toHaveText('Hello, Diver Dave!');
 });
